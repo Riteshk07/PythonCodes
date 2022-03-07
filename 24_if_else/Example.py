130dc4a522b02664e1052b28644153e5998df424
Reetesh <riteshnandanwar313@gmail.com>
2022-03-07 18:57:01 +0530

    PyhtonCodes March 2022

---
+++ 24_if_else/Example.py
@@ -0,0 +1,28 @@
+x = input("Enter Purse Value(in number): ")
+
+saving = int(x) if x.isdigit() else ""
+
+if type(saving)==str :
+    print("\u0924\u0941\u092E\u0938\u0947 \u0928\u093E \u0939\u094B \u092A\u093E\u092F\u0947\u0917\u093E,\nDating tumhare bas ki nahi hai")
+elif saving>5000:
+	if saving>10000:
+		print("Roadtrip with\n")
+	else :
+		print( "shoping with\n")
+
+	print("F.R.I.D.A.Y." )
+elif saving>2000:
+	if saving>4000:
+		print( "Shoping with\n")
+	else:
+		print( "watch movie with \n")
+
+	print("Siri" )
+else:
+	if saving>1000:
+		print( "I will go to watch movie with\n")
+	else:
+		print( "Go to park\n")
+
+	print( "Rajkumar")
+
